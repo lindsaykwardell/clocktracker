e1c07bca715a9c503ae392f2f881724892d6cc6b
Lindsay Wardell <three060@gmail.com>
2023-07-09 14:58:53 -0700

    Add meta tags to public profile

--- pages/@[username]/index.vue
+++ pages/@[username]/index.vue
@@ -70,10 +70,56 @@
 
 <script setup lang="ts">
 import type { Game, Character } from "@prisma/client";
+const route = useRoute();
 const username = useRoute().params.username as string;
+
+const player = await useFetch(`/api/user/${username}`);
 
 useHead({
   title: username,
+  meta: [
+    {
+      hid: "description",
+      name: "description",
+      content: `View ${username}'s profile on ClockTracker`,
+    },
+    {
+      property: "og:title",
+      content: `${username} | ClockTracker`,
+    },
+    {
+      property: "og:description",
+      content: `View ${username}'s profile on ClockTracker`,
+    },
+    {
+      property: "og:image",
+      content: player.data.value?.avatar || "",
+    },
+    {
+      property: "og:url",
+      content: route.fullPath,
+    },
+    {
+      property: "twitter:card",
+      content: "summary_large_image",
+    },
+    {
+      property: "twitter:url",
+      content: route.fullPath,
+    },
+    {
+      property: "twitter:title",
+      content: `${username} | ClockTracker`,
+    },
+    {
+      property: "twitter:description",
+      content: `View ${username}'s profile on ClockTracker`,
+    },
+    {
+      property: "twitter:image",
+      content: player.data.value?.avatar || "",
+    },
+  ],
 });
 
 const games = await useFetch<(Game & { player_characters: Character[] })[]>(
