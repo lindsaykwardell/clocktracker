5e2d7d1458cd0491300abca6e8df435632069126
Lindsay Wardell <three060@gmail.com>
2023-07-09 16:07:32 -0700

    Swap script and role

--- components/GameOverviewGrid.vue
+++ components/GameOverviewGrid.vue
@@ -3,21 +3,29 @@
     <div v-for="game in orderedGames" class="w-full sm:w-1/2 lg:w-1/3 p-2">
       <button
         @click="viewGame(game.id)"
-        class="relative w-full bg-gradient-to-b from-stone-100 hover:from-stone-200 to-stone-300 hover:to-stone-400 flex flex-col items-center cursor-pointer rounded overflow-hidden"
+        class="relative w-full bg-gradient-to-b from-stone-100 hover:from-stone-200 to-stone-400 hover:to-stone-500 flex flex-col items-center cursor-pointer rounded overflow-hidden text-black h-64"
       >
-        <img :src="scriptLogo(game.script)" class="w-48 h-48 object-contain" />
         <div
-          class="absolute bottom-1 right-1 w-20 h-20 text-sm text-black font-julee flex flex-col justify-center items-center rounded-full bg-gradient-to-b from-stone-300 via-stone-300 shadow-xl border border-3"
-          :class="{
-            'to-blue-600 border-blue-800':
-              game.player_characters[0].alignment === 'GOOD',
-            'to-red-600 border-blue-800':
-              game.player_characters[0].alignment === 'EVIL',
-          }"
+          class="flex-grow items-center justify-center flex font-julee text-3xl"
         >
-          {{ game.player_characters[0].name }}
+          <div
+            class="rounded-full w-48 h-48 bg-gradient-to-b from-stone-300 via-stone-300 shadow-xl border border-3 flex items-center justify-center"
+            :class="{
+              'to-blue-600 border-blue-800':
+                game.player_characters[0].alignment === 'GOOD',
+              'to-red-600 border-blue-800':
+                game.player_characters[0].alignment === 'EVIL',
+            }"
+          >
+            {{ game.player_characters[0].name }}
+          </div>
         </div>
-        <div class="w-full p-1 text-black bg-stone-300 text-left">
+        <div
+          class="absolute bottom-1 right-1 w-20 h-20 text-sm text-black font-julee flex flex-col justify-center items-center"
+        >
+          <img :src="scriptLogo(game.script)" class="w-full object-contain" />
+        </div>
+        <div class="w-full p-1 text-black text-left">
           {{ formatDate(game.date) }}
         </div>
       </button>
