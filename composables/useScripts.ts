5af13464986d6648288338c45d30276e140b0ea6
Lindsay Wardell <three060@gmail.com>
2023-07-10 10:39:04 -0700

    Use new script icons

--- composables/useScripts.ts
+++ composables/useScripts.ts
@@ -16,11 +16,11 @@ export const useScripts = () => {
     baseScripts,
     scriptLogo: (script: Script | string) => {
       if (script === Script.TroubleBrewing) {
-        return "/img/trouble-brewing.webp";
+        return "/img/trouble_brewing.png";
       } else if (script === Script.SectsAndViolets) {
-        return "/img/sects-and-violets.webp";
+        return "/img/sects_and_violets.png";
       } else if (script === Script.BadMoonRising) {
-        return "/img/bad-moon-rising.webp";
+        return "/img/bad_moon_rising.png";
       } else {
         return "/img/custom-script.webp";
       }
