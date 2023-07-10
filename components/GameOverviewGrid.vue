95274938dc4545a7b067522839892025ce1ae9fb
Lindsay Wardell <three060@gmail.com>
2023-07-09 18:37:31 -0700

    Improved date view when there's a background

--- components/GameOverviewGrid.vue
+++ components/GameOverviewGrid.vue
@@ -3,7 +3,7 @@
     <div v-for="game in orderedGames" class="w-full sm:w-1/2 lg:w-1/3 p-2">
       <button
         @click="viewGame(game.id)"
-        class="relative w-full bg-gradient-to-b from-stone-100 hover:from-stone-200 to-stone-400 hover:to-stone-500 flex flex-col items-center cursor-pointer rounded overflow-hidden text-black h-64"
+        class="relative w-full bg-gradient-to-b from-stone-100 hover:from-stone-200 to-stone-400 hover:to-stone-500 flex flex-col items-center cursor-pointer rounded overflow-hidden text-black h-72"
       >
         <img
           v-if="game.image_urls[0]"
@@ -30,12 +30,14 @@
           </div>
         </div>
         <div
+          class="absolute bottom-0 w-full p-1 text-black text-left bg-stone-300"
+        >
+          {{ formatDate(game.date) }}
+        </div>
+        <div
           class="absolute bottom-1 right-1 w-20 h-20 text-sm text-black font-julee flex flex-col justify-center items-center"
         >
           <img :src="scriptLogo(game.script)" class="w-full object-contain" />
-        </div>
-        <div class="w-full p-1 text-black text-left">
-          {{ formatDate(game.date) }}
         </div>
       </button>
     </div>
