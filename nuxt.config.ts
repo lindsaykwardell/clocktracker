1d632b671206af5b67f783e2a0161e4397b54639
Lindsay Wardell <three060@gmail.com>
2023-07-14 13:00:26 -0700

    Basic manifest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/supabase"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/supabase", "@vite-pwa/nuxt"],
   app: {
     head: {
       charset: "utf-8",
@@ -22,6 +22,44 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       sentryDsn: process.env.SENTRY_DSN,
+    },
+  },
+  pwa: {
+    registerType: "autoUpdate",
+    includeAssets: [
+      "/img/**",
+      "/logo-ct-sm.png",
+      "logo.png",
+      "apple-touch-icon.png",
+    ],
+    manifest: {
+      name: "ClockTracker",
+      short_name: "ClockTracker",
+      description: "Record your game details from Blood on the Clocktower",
+      theme_color: "#1C1917",
+      icons: [
+        {
+          src: "pwa-128.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "pwa-128.png",
+          sizes: "128x128",
+          type: "image/png",
+        },
+        {
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+      ],
     },
   },
 });
