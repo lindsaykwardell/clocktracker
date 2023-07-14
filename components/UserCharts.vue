d597552ee899af6c9029009337e882259b9afb7c
Lindsay Wardell <three060@gmail.com>
2023-07-14 15:05:27 -0700

    Add space below charts

--- components/UserCharts.vue
+++ components/UserCharts.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="flex flex-col items-center justify-center sm:flex-row flex-wrap gap-y-12 pb-4">
+  <div
+    class="flex flex-col items-center justify-center sm:flex-row flex-wrap gap-y-12"
+  >
     <GamesOverTime :games="games" class="h-[250px] w-screen md:w-3/5" />
     <TopCharacters :games="games" class="sm:w-1/2 md:w-2/5 pl-4" />
     <WinRateByRole :games="games" class="h-[250px] w-screen md:w-1/3" />
     <RoleType :games="games" class="h-[250px] w-screen md:w-1/3" />
   </div>
+  <!-- Hack, I'm sorry future me -->
+  <div class="h-[30px]">&nbsp;</div>
 </template>
 
 <script setup lang="ts">
