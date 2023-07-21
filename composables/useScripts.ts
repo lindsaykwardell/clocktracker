434a5b4bb0b295eed71cd884fff0a8aa697fab84
Lindsay Wardell <three060@gmail.com>
2023-07-20 20:17:04 -0400

    Fix sects & violets to be in sync with app

--- composables/useScripts.ts
+++ composables/useScripts.ts
@@ -1,6 +1,6 @@
 enum Script {
   TroubleBrewing = "Trouble Brewing",
-  SectsAndViolets = "Sects & Violets",
+  SectsAndViolets = "Sects and Violets",
   BadMoonRising = "Bad Moon Rising",
   CustomScript = "Custom Script",
 }
@@ -18,7 +18,10 @@ export const useScripts = () => {
     scriptLogo: (script: Script | string) => {
       if (script === Script.TroubleBrewing) {
         return "/img/trouble_brewing.png";
-      } else if (script === Script.SectsAndViolets) {
+      } else if (
+        script === Script.SectsAndViolets ||
+        script == "Sects & Violets"
+      ) {
         return "/img/sects_and_violets.png";
       } else if (script === Script.BadMoonRising) {
         return "/img/bad_moon_rising.png";
