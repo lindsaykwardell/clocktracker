09875fa8470de3692b3427f2deaee96c7c0a2f4e
Lindsay Wardell <three060@gmail.com>
2023-08-10 11:00:30 -0700

    Enable loading view for friends

--- pages/friends.vue
+++ pages/friends.vue
@@ -1,37 +1,52 @@
 <template>
   <AuthenticatedTemplate>
-    <template v-if="friends.getFriendRequests.length > 0">
-      <p class="text-center text-2xl my-4 font-dumbledor">Friend Requests</p>
+    <template
+      v-if="
+        friends.friends.status === Status.SUCCESS &&
+        friends.requests.status === Status.SUCCESS
+      "
+    >
+      <template v-if="friends.getFriendRequests.length > 0">
+        <p class="text-center text-2xl my-4 font-dumbledor">Friend Requests</p>
+        <UserCard
+          v-for="request in friends.getFriendRequests"
+          :username="
+            request.user_id === user?.id
+              ? request.from_user.username
+              : request.user.username
+          "
+        >
+          <div class="p-2">
+            <FriendButton
+              :username="
+                request.user_id === user?.id
+                  ? request.from_user.username
+                  : request.user.username
+              "
+              :user_id="
+                request.user_id === user?.id
+                  ? request.from_user.user_id
+                  : request.user.user_id
+              "
+            />
+          </div>
+        </UserCard>
+      </template>
+      <p class="text-center text-2xl my-4 font-dumbledor">Friends</p>
       <UserCard
-        v-for="request in friends.getFriendRequests"
-        :username="
-          request.user_id === user?.id
-            ? request.from_user.username
-            : request.user.username
-        "
+        v-for="friend in friends.getFriends"
+        :username="friend.username"
       >
         <div class="p-2">
-          <FriendButton
-            :username="
-              request.user_id === user?.id
-                ? request.from_user.username
-                : request.user.username
-            "
-            :user_id="
-              request.user_id === user?.id
-                ? request.from_user.user_id
-                : request.user.user_id
-            "
-          />
+          <FriendButton :username="friend.username" :user_id="friend.user_id" />
         </div>
       </UserCard>
     </template>
-    <p class="text-center text-2xl my-4 font-dumbledor">Friends</p>
-    <UserCard v-for="friend in friends.getFriends" :username="friend.username">
-      <div class="p-2">
-        <FriendButton :username="friend.username" :user_id="friend.user_id" />
+    <template v-else>
+      <div class="w-full h-screen flex justify-center items-center">
+        <Loading />
       </div>
-    </UserCard>
+    </template>
   </AuthenticatedTemplate>
 </template>
 
