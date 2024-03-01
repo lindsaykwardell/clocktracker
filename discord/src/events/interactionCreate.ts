7367e00473d0b7267a00701cb1253184dc626b81
Lindsay Wardell <three060@gmail.com>
2024-03-01 14:52:31 -0800

    Make event interaction work after bot restart

--- discord/src/events/interactionCreate.ts
+++ discord/src/events/interactionCreate.ts
@@ -1,30 +1,35 @@
 import { Events } from "discord.js";
+import { handleRegisterButtonClick } from "../commands/community/get-event";
 
 export const name = Events.InteractionCreate;
 export async function execute(interaction) {
-  if (!interaction.isChatInputCommand()) return;
+  if (interaction.isChatInputCommand()) {
+    const command = interaction.client.commands.get(interaction.commandName);
 
-  const command = interaction.client.commands.get(interaction.commandName);
-
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
-  }
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
-    } else {
-      await interaction.reply({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
+    if (!command) {
+      console.error(
+        `No command matching ${interaction.commandName} was found.`
+      );
+      return;
     }
+
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      console.error(error);
+      if (interaction.replied || interaction.deferred) {
+        await interaction.followUp({
+          content: "There was an error while executing this command!",
+          ephemeral: true,
+        });
+      } else {
+        await interaction.reply({
+          content: "There was an error while executing this command!",
+          ephemeral: true,
+        });
+      }
+    }
+  } else if (interaction.isButton()) {
+    handleRegisterButtonClick(interaction);
   }
 }
