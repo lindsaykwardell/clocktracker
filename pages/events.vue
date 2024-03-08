f1dcf0c1a62787f6ea1fc80eefd79ca19582de47
Lindsay Wardell <three060@gmail.com>
2024-03-08 10:14:39 -0700

    Fix events page

--- pages/events.vue
+++ pages/events.vue
@@ -1,21 +1,23 @@
 <template>
   <StandardTemplate>
-    <template v-if="events.length">
-      <section class="flex flex-col gap-2 items-center">
-        <h1 class="text-3xl font-bold mb-8 font-dumbledor py-4">
-          My Upcoming Events
-        </h1>
-        <div class="w-full md:w-[600px] flex flex-col gap-4">
-          <nuxt-link
-            v-for="event in events"
-            :to="`/community/${event.community.slug}/event/${event.id}`"
-            class="w-full md:w-[600px]"
-          >
-            <EventCard :event="event" />
-          </nuxt-link>
-        </div>
-      </section>
-    </template>
+    <section class="flex flex-col gap-2 items-center">
+      <h1 class="text-3xl font-bold mb-8 font-dumbledor py-4">
+        My Upcoming Events
+      </h1>
+      <div class="w-full md:w-[600px] flex flex-col gap-4">
+        <nuxt-link
+          v-for="event in events"
+          :to="`/community/${event.community.slug}/event/${event.id}`"
+          class="w-full md:w-[600px]"
+        >
+          <EventCard :event="event" />
+        </nuxt-link>
+      </div>
+      <div v-if="!events.length">
+        No upcoming events. Check out your communities for events to register
+        for!
+      </div>
+    </section>
   </StandardTemplate>
 </template>
 
