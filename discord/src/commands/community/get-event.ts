57491f80a81b284d3f98ecccb9e991146a6e751c
Lindsay Wardell <three060@gmail.com>
2024-03-10 16:30:12 -0700

    Added autocomplete to getting events

--- discord/src/commands/community/get-event.ts
+++ discord/src/commands/community/get-event.ts
@@ -1,12 +1,58 @@
 import { CacheType, CommandInteraction, SlashCommandBuilder } from "discord.js";
 import { replyWithEvent, findEvent } from "../../utility/community-event";
+import { PrismaClient } from "@prisma/client";
+import dayjs from "dayjs";
+
+const prisma = new PrismaClient();
 
 export const data = new SlashCommandBuilder()
   .setName("get-event")
   .setDescription("Get an event by its ID")
   .addStringOption((option) =>
-    option.setName("id").setDescription("The ID of the event").setRequired(true)
+    option
+      .setName("id")
+      .setDescription("The ID of the event")
+      .setRequired(true)
+      .setAutocomplete(true)
   );
+
+export async function autocomplete(interaction) {
+  const guild_id = interaction.guildId;
+  const focusedValue = interaction.options.getFocused().toLowerCase();
+
+  const choices = await prisma.event.findMany({
+    where: {
+      community: {
+        discord_server_id: guild_id,
+      },
+      end: {
+        gte: new Date(),
+      },
+      title: {
+        contains: focusedValue,
+        mode: "insensitive",
+      },
+    },
+    take: 25,
+    orderBy: {
+      start: "asc",
+    },
+    select: {
+      id: true,
+      title: true,
+      start: true,
+    },
+  });
+
+  await interaction.respond(
+    choices.map((choice) => ({
+      name: `${choice.title} - ${dayjs(choice.start).format(
+        "YYYY-MM-DD HH:mm"
+      )}`,
+      value: choice.id,
+    }))
+  );
+}
 
 export async function execute(interaction: CommandInteraction<CacheType>) {
   const event_id = interaction.options.get("id").value as string;
