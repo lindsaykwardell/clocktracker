0332bed0545d5a48df9f203920965fee814b8b08
Lindsay Wardell <three060@gmail.com>
2024-03-18 21:43:16 -0700

    Add a way to push announcements into the app

---
+++ components/AnnouncementDialog.vue
@@ -0,0 +1,26 @@
+<template>
+  <Dialog v-model:visible="show" size="md">
+    <template #title>
+      <slot name="title" />
+    </template>
+    <div class="p-4">
+      <slot />
+    </div>
+  </Dialog>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  id: string;
+}>();
+
+const show = ref(false);
+
+onMounted(() => {
+  const latestAnnouncement = localStorage.getItem("announcement");
+  if (latestAnnouncement !== props.id) {
+    show.value = true;
+    localStorage.setItem("announcement", props.id);
+  }
+});
+</script>
