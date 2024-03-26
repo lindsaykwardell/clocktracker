cc0e2b3878708d689f25573a8ba54b2f2d1a53f3
Lindsay Wardell <three060@gmail.com>
2024-03-25 19:46:18 -0700

    Move players around event lists

---
+++ server/api/community/[slug]/event/[event_id]/registered.put.ts
@@ -0,0 +1,127 @@
+import { PrismaClient } from "@prisma/client";
+import { User } from "@supabase/supabase-js";
+import { fetchEventAndUpdateDiscord } from "~/server/utils/fetchEventAndUpdateDiscord";
+
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (handler) => {
+  const me: User | null = handler.context.user;
+  const slug = handler.context.params!.slug;
+  const event_id = handler.context.params!.event_id;
+  const body = await readBody<{
+    player_id: number;
+    from: "registered" | number;
+    to: "registered" | number;
+  } | null>(handler);
+
+  if (!me) {
+    throw createError({
+      status: 401,
+      statusMessage: "Unauthorized",
+    });
+  }
+
+  if (!body) {
+    throw createError({
+      status: 400,
+      statusMessage: "Bad Request",
+    });
+  }
+
+  const event = await prisma.event.findUnique({
+    where: {
+      id: event_id,
+      community: {
+        slug,
+        admins: {
+          some: {
+            user_id: me?.id || "",
+          },
+        },
+      },
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  if (!event) {
+    throw createError({
+      status: 404,
+      statusMessage: "Not Found",
+    });
+  }
+
+  if (body.from === "registered") {
+    // The player is currently registered, and is being moved to a waitlist.
+    const player = await prisma.eventAttendee.findUnique({
+      where: {
+        id: body.player_id,
+      },
+    });
+
+    if (!player) {
+      throw createError({
+        status: 400,
+        statusMessage: "Bad Request",
+      });
+    }
+
+    await prisma.eventWaitlistAttendee.create({
+      data: {
+        waitlist_id: body.to as number,
+        name: player.name,
+        user_id: player.user_id,
+        discord_user_id: player.discord_user_id,
+      },
+    });
+
+    await prisma.eventAttendee.delete({
+      where: {
+        id: player.id,
+      },
+    });
+  } else {
+    // A player is being moved from a waitlist.
+    const player = await prisma.eventWaitlistAttendee.findUnique({
+      where: {
+        id: body.player_id,
+      },
+    });
+
+    if (!player) {
+      throw createError({
+        status: 400,
+        statusMessage: "Bad Request",
+      });
+    }
+
+    if (body.to === "registered") {
+      await prisma.eventAttendee.create({
+        data: {
+          event_id,
+          user_id: player.user_id,
+          name: player.name,
+          discord_user_id: player.discord_user_id,
+        },
+      });
+
+      await prisma.eventWaitlistAttendee.delete({
+        where: {
+          id: player.id,
+        },
+      });
+    } else {
+      await prisma.eventWaitlistAttendee.update({
+        where: {
+          id: player.id,
+        },
+        data: {
+          waitlist_id: body.to as number,
+        },
+      });
+    }
+  }
+
+  return fetchEventAndUpdateDiscord(event_id);
+});
