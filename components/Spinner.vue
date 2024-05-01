b0e15b631dddd0398cd8776a18edbbc9c7d0d072
Lindsay Wardell <three060@gmail.com>
2024-05-01 15:35:02 -0700

    Enable spam protection for registering for events on ClockTracker

--- components/Spinner.vue
+++ components/Spinner.vue
@@ -1,6 +1,6 @@
 <template>
   <svg
-    class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+    class="animate-spin -ml-1 h-5 w-5 text-white"
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
--- server/middleware/log.ts
+++ server/middleware/log.ts
@@ -1,3 +1,4 @@
 export default defineEventHandler((event) => {
-  console.log(`${event.node.req.method}: ${getRequestURL(event)}`);
+  const ip_address = getRequestIP(event, { xForwardedFor: true});
+  console.log(`${ip_address} [${event.node.req.method}]: ${getRequestURL(event)}`);
 });
