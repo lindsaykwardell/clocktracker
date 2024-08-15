89e7c24784eaf8ad5fb82a7c460aa09f42f23794
Lindsay Wardell <three060@gmail.com>
2024-08-14 21:30:22 -0700

    Enable selecting multiple games

--- components/GameOverviewGrid.vue
+++ components/GameOverviewGrid.vue
@@ -2,130 +2,149 @@
   <div class="flex flex-wrap">
     <div
       v-for="game in games"
-      class="border border-black relative"
-      :class="cardWidth"
+      class="border border-black transition duration-150"
+      :class="
+        cardWidth +
+        (selectMultipleGames.selectedGames.includes(game.id)
+          ? ' bg-primary'
+          : '')
+      "
     >
-      <component
-        :is="componentIs"
-        @click="handleCardClick(game)"
-        :to="`/game/${game.id}`"
-        class="w-full bg-stone-900 flex flex-col items-center cursor-pointer rounded overflow-hidden text-black h-48 md:h-72 bg-cover bg-center"
+      <div
+        class="relative transition duration-150"
         :class="{
-          'trouble-brewing': game.script === 'Trouble Brewing',
-          'sects-and-violets': game.script === 'Sects and Violets',
-          'bad-moon-rising': game.script === 'Bad Moon Rising',
-          'custom-script':
-            ['Trouble Brewing', 'Sects and Violets', 'Bad Moon Rising'].indexOf(
-              game.script
-            ) === -1,
+          'scale-90': selectMultipleGames.enabled,
         }"
       >
-        <img
-          v-if="game.image_urls[0]"
-          :src="fullImageUrl(game.image_urls[0])"
-          class="absolute bottom-0 w-full h-full object-cover blur-sm"
-          crossorigin="anonymous"
-        />
-        <div
-          class="absolute top-0 left-0 text-white md:text-lg bg-gradient-to-br from-black/75 via-black/50 to-black-0 p-1"
+        <button
+          class="absolute top-0 left-0 w-full h-full z-50 cursor-pointer"
+          @click="selectMultipleGames.toggleGame(game.id)"
+        ></button>
+        <component
+          :is="componentIs"
+          @click="handleCardClick(game)"
+          :to="`/game/${game.id}`"
+          class="w-full bg-stone-900 flex flex-col items-center cursor-pointer rounded overflow-hidden text-black h-48 md:h-72 bg-cover bg-center"
+          :class="{
+            'trouble-brewing': game.script === 'Trouble Brewing',
+            'sects-and-violets': game.script === 'Sects and Violets',
+            'bad-moon-rising': game.script === 'Bad Moon Rising',
+            'custom-script':
+              [
+                'Trouble Brewing',
+                'Sects and Violets',
+                'Bad Moon Rising',
+              ].indexOf(game.script) === -1,
+          }"
         >
-          {{ formatDate(game.date) }}
-        </div>
-        <div class="absolute top-8 left-1 z-10">
-          <Avatar
-            v-if="game.community"
-            :value="game.community.icon"
-            size="sm"
-            class="flex-shrink bg-stone-300 dark:bg-stone-950"
+          <img
+            v-if="game.image_urls[0]"
+            :src="fullImageUrl(game.image_urls[0])"
+            class="absolute bottom-0 w-full h-full object-cover blur-sm"
+            crossorigin="anonymous"
           />
-        </div>
-        <div
-          class="flex-grow items-center justify-center flex font-dumbledor text-3xl"
-        >
-          <Token
-            v-if="
-              gamesStore.getLastCharater(game.id).name ||
-              gamesStore.getLastCharater(game.id).alignment !== 'NEUTRAL'
-            "
-            :character="gamesStore.getLastCharater(game.id)"
-            size="lg"
-          />
-          <div v-else class="bg-black/25 flex justify-center">
-            <img
-              :src="
-                game.associated_script?.logo ?? scripts.scriptLogo(game.script)
-              "
-              class="w-2/3"
+          <div
+            class="absolute top-0 left-0 text-white md:text-lg bg-gradient-to-br from-black/75 via-black/50 to-black-0 p-1"
+          >
+            {{ formatDate(game.date) }}
+          </div>
+          <div class="absolute top-8 left-1 z-10">
+            <Avatar
+              v-if="game.community"
+              :value="game.community.icon"
+              size="sm"
+              class="flex-shrink bg-stone-300 dark:bg-stone-950"
             />
           </div>
-        </div>
-        <div class="absolute w-full top-0 left-0" />
-        <img
-          class="absolute top-2 right-2 w-8 h-8 md:w-12 md:h-12"
-          :src="displayWinIcon(game)"
-        />
-      </component>
-      <div
-        class="absolute bottom-0 left-0 w-full flex justify-between bg-gradient-to-tr from-black/75 via-black/25 to-black/75"
-      >
-        <div
-          class="absolute -top-10 left-0 p-1 flex gap-2 items-center bg-gradient-to-r from-black/75 via-black/50 to-black-0"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 256 256"
-            class="text-yellow-400"
+          <div
+            class="flex-grow items-center justify-center flex font-dumbledor text-3xl"
           >
-            <path
-              fill="currentColor"
-              d="M64.12 147.8a4 4 0 0 1-4 4.2H16a8 8 0 0 1-7.8-6.17a8.35 8.35 0 0 1 1.62-6.93A67.8 67.8 0 0 1 37 117.51a40 40 0 1 1 66.46-35.8a3.94 3.94 0 0 1-2.27 4.18A64.08 64.08 0 0 0 64 144c0 1.28 0 2.54.12 3.8m182-8.91A67.76 67.76 0 0 0 219 117.51a40 40 0 1 0-66.46-35.8a3.94 3.94 0 0 0 2.27 4.18A64.08 64.08 0 0 1 192 144c0 1.28 0 2.54-.12 3.8a4 4 0 0 0 4 4.2H240a8 8 0 0 0 7.8-6.17a8.33 8.33 0 0 0-1.63-6.94Zm-89 43.18a48 48 0 1 0-58.37 0A72.13 72.13 0 0 0 65.07 212A8 8 0 0 0 72 224h112a8 8 0 0 0 6.93-12a72.15 72.15 0 0 0-33.74-29.93Z"
-            />
-          </svg>
-          <div class="text-white font-bold">
-            {{ game.player_count }}
-            <template v-if="game.traveler_count">
-              (+{{ game.traveler_count }})
-            </template>
-          </div>
-        </div>
-        <div class="flex-grow flex gap-1">
-          <a
-            v-if="game.bgg_id"
-            target="_blank"
-            :href="`https://boardgamegeek.com/play/details/${game.bgg_id}`"
-            class="w-12 md:w-8"
-          >
-            <img src="/img/bgg.png" class="w-8 h-8 aspect-square" />
-          </a>
-          <div class="font-gothic text-white md:text-lg p-1">
-            {{ game.script }}
-            <template
+            <Token
               v-if="
-                game.associated_script && !scripts.isBaseScript(game.script)
+                gamesStore.getLastCharater(game.id).name ||
+                gamesStore.getLastCharater(game.id).alignment !== 'NEUTRAL'
               "
-            >
-              v{{ game.associated_script.version }}
-            </template>
-          </div>
-        </div>
-        <nuxt-link
-          v-if="!readonly"
-          class="text-white font-bold px-4 rounded inline-flex items-center justify-center gap-1 flex-1 md:flex-initial"
-          :to="`/game/${game.id}/edit`"
-          ><svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 32 32"
-          >
-            <path
-              fill="currentColor"
-              d="M2 26h28v2H2zM25.4 9c.8-.8.8-2 0-2.8l-3.6-3.6c-.8-.8-2-.8-2.8 0l-15 15V24h6.4l15-15zm-5-5L24 7.6l-3 3L17.4 7l3-3zM6 22v-3.6l10-10l3.6 3.6l-10 10H6z"
+              :character="gamesStore.getLastCharater(game.id)"
+              size="lg"
             />
-          </svg>
-        </nuxt-link>
+            <div v-else class="bg-black/25 flex justify-center">
+              <img
+                :src="
+                  game.associated_script?.logo ??
+                  scripts.scriptLogo(game.script)
+                "
+                class="w-2/3"
+              />
+            </div>
+          </div>
+          <div class="absolute w-full top-0 left-0" />
+          <img
+            class="absolute top-2 right-2 w-8 h-8 md:w-12 md:h-12"
+            :src="displayWinIcon(game)"
+          />
+        </component>
+        <div
+          class="absolute bottom-0 left-0 w-full flex justify-between bg-gradient-to-tr from-black/75 via-black/25 to-black/75"
+        >
+          <div
+            class="absolute -top-10 left-0 p-1 flex gap-2 items-center bg-gradient-to-r from-black/75 via-black/50 to-black-0"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 256 256"
+              class="text-yellow-400"
+            >
+              <path
+                fill="currentColor"
+                d="M64.12 147.8a4 4 0 0 1-4 4.2H16a8 8 0 0 1-7.8-6.17a8.35 8.35 0 0 1 1.62-6.93A67.8 67.8 0 0 1 37 117.51a40 40 0 1 1 66.46-35.8a3.94 3.94 0 0 1-2.27 4.18A64.08 64.08 0 0 0 64 144c0 1.28 0 2.54.12 3.8m182-8.91A67.76 67.76 0 0 0 219 117.51a40 40 0 1 0-66.46-35.8a3.94 3.94 0 0 0 2.27 4.18A64.08 64.08 0 0 1 192 144c0 1.28 0 2.54-.12 3.8a4 4 0 0 0 4 4.2H240a8 8 0 0 0 7.8-6.17a8.33 8.33 0 0 0-1.63-6.94Zm-89 43.18a48 48 0 1 0-58.37 0A72.13 72.13 0 0 0 65.07 212A8 8 0 0 0 72 224h112a8 8 0 0 0 6.93-12a72.15 72.15 0 0 0-33.74-29.93Z"
+              />
+            </svg>
+            <div class="text-white font-bold">
+              {{ game.player_count }}
+              <template v-if="game.traveler_count">
+                (+{{ game.traveler_count }})
+              </template>
+            </div>
+          </div>
+          <div class="flex-grow flex gap-1">
+            <a
+              v-if="game.bgg_id"
+              target="_blank"
+              :href="`https://boardgamegeek.com/play/details/${game.bgg_id}`"
+              class="w-12 md:w-8"
+            >
+              <img src="/img/bgg.png" class="w-8 h-8 aspect-square" />
+            </a>
+            <div class="font-gothic text-white md:text-lg p-1">
+              {{ game.script }}
+              <template
+                v-if="
+                  game.associated_script && !scripts.isBaseScript(game.script)
+                "
+              >
+                v{{ game.associated_script.version }}
+              </template>
+            </div>
+          </div>
+          <nuxt-link
+            v-if="!readonly"
+            class="text-white font-bold px-4 rounded inline-flex items-center justify-center gap-1 flex-1 md:flex-initial"
+            :to="`/game/${game.id}/edit`"
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 32 32"
+            >
+              <path
+                fill="currentColor"
+                d="M2 26h28v2H2zM25.4 9c.8-.8.8-2 0-2.8l-3.6-3.6c-.8-.8-2-.8-2.8 0l-15 15V24h6.4l15-15zm-5-5L24 7.6l-3 3L17.4 7l3-3zM6 22v-3.6l10-10l3.6 3.6l-10 10H6z"
+              />
+            </svg>
+          </nuxt-link>
+        </div>
       </div>
     </div>
   </div>
@@ -137,6 +156,7 @@ const gamesStore = useGames();
 
 const config = useRuntimeConfig();
 const scripts = useScripts();
+const selectMultipleGames = useSelectMultipleGames();
 
 const props = defineProps<{
   games: GameRecord[];
