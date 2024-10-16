bd56f1b479d624ba480f28591d2854e376db2a34
Lindsay Wardell <three060@gmail.com>
2024-10-15 21:36:46 -0700

    Enable locations for communities

--- components/CommunityTemplate.vue
+++ components/CommunityTemplate.vue
@@ -1,7 +1,7 @@
 <template>
   <StandardTemplate>
     <template v-if="community.status === Status.SUCCESS">
-      <div class=" dark:bg-stone-950 shadow-lg">
+      <div class="dark:bg-stone-950 shadow-lg">
         <div
           class="flex flex-col items-center w-full md:w-3/4 lg:w-2/3 xl:w-1/2 m-auto"
         >
@@ -16,6 +16,23 @@
                 <h1 class="font-dumbledor text-1xl lg:text-2xl">
                   {{ community.data.name }}
                 </h1>
+                <div
+                  v-if="community.data.location"
+                  class="md:text-lg dark:text-stone-400 flex gap-2 items-center"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="32"
+                    height="32"
+                    viewBox="0 0 32 32"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M16 2a14 14 0 1 0 14 14A14.016 14.016 0 0 0 16 2ZM4.02 16.394l1.338.446L7 19.303v1.283a1 1 0 0 0 .293.707L10 24v2.377a11.994 11.994 0 0 1-5.98-9.983ZM16 28a11.968 11.968 0 0 1-2.572-.285L14 26l1.805-4.512a1 1 0 0 0-.097-.926l-1.411-2.117a1 1 0 0 0-.832-.445h-4.93l-1.248-1.873L9.414 14H11v2h2v-2.734l3.868-6.77l-1.736-.992L14.277 7h-2.742L10.45 5.371A11.861 11.861 0 0 1 20 4.7V8a1 1 0 0 0 1 1h1.465a1 1 0 0 0 .832-.445l.877-1.316A12.033 12.033 0 0 1 26.894 11H22.82a1 1 0 0 0-.98.804l-.723 4.47a1 1 0 0 0 .54 1.055L25 19l.685 4.056A11.98 11.98 0 0 1 16 28Z"
+                    />
+                  </svg>
+                  <span>{{ community.data.location }}</span>
+                </div>
               </div>
               <button
                 v-if="user && !isBanned"
@@ -34,9 +51,13 @@
               </button>
             </div>
           </div>
-          <div class="flex gap-2">
-            <SocialLink v-for="link in community.data.links" :href="link" class="hover:text-primary dark:hover:text-primary-light transition duration-150 hover:underline">
-              {{ link }}
+          <div class="flex gap-2 flex-col md:flex-row md:flex-wrap">
+            <SocialLink
+              v-for="link in community.data.links"
+              :href="link"
+              class="hover:text-primary dark:hover:text-primary-light transition duration-150 hover:underline"
+            >
+              {{ link.replace(/https?:\/\//, "").replace(/\/$/, "") }}
             </SocialLink>
           </div>
           <hr class="border-stone-100 w-full my-4" />
