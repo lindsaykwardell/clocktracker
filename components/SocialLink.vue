66355b6559660e5d403b4a42524fe4c90826cebe
Lindsay Wardell <three060@gmail.com>
2024-10-19 08:43:22 -0700

    Add Bluesky

--- components/SocialLink.vue
+++ components/SocialLink.vue
@@ -1,12 +1,7 @@
 <template>
   <div class="flex gap-1 items-center">
     <component :is="icon" class="w-6 h-6" :class="iconClass" />
-    <a
-      :href="url"
-      :class="linkClass"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
+    <a :href="url" :class="linkClass" target="_blank" rel="noopener noreferrer">
       <slot />
     </a>
   </div>
@@ -29,6 +24,7 @@ const twitch = resolveComponent("twitch");
 const youtube = resolveComponent("youtube");
 const discord = resolveComponent("discord");
 const facebook = resolveComponent("facebook");
+const bluesky = resolveComponent("bluesky");
 const link = resolveComponent("website");
 
 const icon = computed(() => {
@@ -38,6 +34,7 @@ const icon = computed(() => {
   if (href.includes("twitch.tv")) return twitch;
   if (href.includes("youtube.com")) return youtube;
   if (href.includes("discord.gg")) return discord;
+  if (href.includes("bsky.app")) return bluesky;
   if (href.includes("facebook.com")) return facebook;
   return link;
 });
