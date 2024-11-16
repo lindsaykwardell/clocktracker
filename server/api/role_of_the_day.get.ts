fcf5543b05e8a66192724b02365a606a00855b97
Lindsay Wardell <three060@gmail.com>
2024-11-16 09:19:31 -0800

    Refactor role of the day

--- server/api/role_of_the_day.get.ts
+++ server/api/role_of_the_day.get.ts
@@ -1,31 +1,5 @@
-import { PrismaClient } from "@prisma/client";
-import { User } from "@supabase/supabase-js";
-import dayjs from "dayjs";
-
-const prisma = new PrismaClient();
+import { roleOfTheDay } from "../utils/roleOfTheDay";
 
 export default defineEventHandler(async (handler) => {
-  const roleCount = await prisma.role.count({
-    where: {
-      custom_role: false,
-    },
-  });
-
-  const day = new Date().getDate();
-
-  const roleIndex =
-    dayjs()
-      .startOf("day")
-      .add((day % 7) * (day / 30), "seconds")
-      .add(day, "minutes")
-      .unix() % roleCount;
-
-  const role = await prisma.role.findFirst({
-    where: {
-      custom_role: false,
-    },
-    skip: roleIndex,
-  });
-
-  return role;
+  return roleOfTheDay();
 });
