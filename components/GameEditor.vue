2212d42eb14777010688e86f8b84b128eebea5d7
Lindsay Wardell <three060@gmail.com>
2024-12-04 13:59:44 -0800

    Better error handling

--- components/GameEditor.vue
+++ components/GameEditor.vue
@@ -554,10 +554,10 @@
                 d="m19.7 4.44l-2.38.64L19.65 0L4.53 5.56l.83 6.67l-1.4 1.34L8.12 24l8.85-3.26l3.07-7.22l-1.32-1.27l.98-7.81Z"
               />
             </svg>
-            <Input type="text" v-model="bggIdInput" />
+            <Input type="text" v-model="bggIdInput" pattern="https:\/\/boardgamegeek\.com\/play\/details\/\d+" />
           </div>
           <div v-if="!bggIdIsValid" class="text-red-500">
-            Invalid BoardGameGeek URL
+            Invalid BoardGameGeek URL. Format should be: https://boardgamegeek.com/play/details/[game id]
           </div>
         </label>
       </div>
