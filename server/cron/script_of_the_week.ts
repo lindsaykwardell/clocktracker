ddf368cbf78043c926f84086254b4396814f91ff
Lindsay Wardell <lindsay.wardell@mangomint.com>
2025-02-12 14:56:46 -0800

    Cleanup (but not done)

--- server/cron/script_of_the_week.ts
+++ server/cron/script_of_the_week.ts
@@ -4,6 +4,7 @@ import { roleOfTheDay } from "../utils/roleOfTheDay";
 import { AtpAgent, RichText } from "@atproto/api";
 import { Alignment, WinStatus_V2 } from "@prisma/client";
 import { performLockedTask } from "../utils/cronLock";
+import scripts_of_the_weekGet from "../api/scripts_of_the_week.get";
 
 const agent = new AtpAgent({
   service: "https://bsky.social",
@@ -25,60 +26,11 @@ export default defineCronHandler(
         password: process.env.BSKY_PASSWORD,
       });
 
-      // Translate the following SQL into a Prisma query
-      /**
-       * select
-  script,
-  "Game".script_id,
-  count(*)
-from
-  "Game"
-  left join "Script" on "Game".script_id = "Script".id
-where
-  parent_game_id is not null
-  and (
-    "Script".author is null
-    or "Script".author != 'The Pandemonium Institute'
-  )
-  and script != ''
-  and script != 'Custom'
-  and to_char(created_at, 'YYYY-MM') = '2024-04'
-group by
-  script,
-  "Game".script_id
-order by
-  count desc
-limit
-  5;
-       */
+      const scripts = await getScriptsOfTheWeek(prisma);
 
-      const games = await prisma.game.findMany({
-        where: {
-          parent_game_id: null,
-          script: {
-            not: "",
-          },
-          AND: {
-            script: {
-              not: "Custom",
-            },
-          },
-          // associated_script: {
-          //   author: {
-          //     not: "The Pandemonium Institute",
-          //   },
-          // },
-          created_at: {
-            gte: dayjs().subtract(1, "week").toDate(),
-          },
-        },
-        select: {
-          script: true,
-          script_id: true,
-        },
-      });
-
-      console.log(games);
+      if (scripts.length === 0) {
+        return;
+      }
     }),
   {
     timeZone: "America/New_York",
