0c3073875fe1ba98139648c747b016a461abef42
Lindsay Wardell <lindsay.wardell@mangomint.com>
2025-06-13 14:47:36 -0700

    Add hermit announcement

--- app.vue
+++ app.vue
@@ -1,69 +1,46 @@
 <template>
   <NuxtPage />
-  <AnnouncementDialog id="fleshandbone-040525-">
+  <AnnouncementDialog id="hermit-added">
     <template #title>
-      <h1 class="text-2xl font-bold font-sorts">
-        Announcing "Living Scripts" integration!
-      </h1>
+      <h1 class="text-2xl font-bold font-sorts">Hermit Added!</h1>
       <div class="text-lg text-stone-400">{{ formattedAnnouncementDate }}</div>
     </template>
-    <p class="p-2">
-      A new integration is now available for ClockTracker:
-      <strong>"Living Scripts"</strong>!
-    </p>
-    <p class="p-2">From the Living Scripts website:</p>
-    <p
-      class="p-2 pl-6 border-l-4 border-stone-300 dark:border-stone-700 text-stone-600 dark:text-stone-400 italic"
-    >
-      <em>
-        If you've ever gathered your group to play Blood on the Clocktower and
-        wondered, "What script should we play?"—you're in the right place.
-        Living Scripts takes the game you love and transforms it into an
-        evolving, dynamic campaign system that builds tension, bonds, and a
-        story you'll remember long after the games end. Think of it as Blood on
-        the Clocktower—but with consequences that ripple across multiple
-        sessions.
-      </em>
-    </p>
-    <p class="p-2">
-      Living Scripts is a new way to play Blood on the Clocktower that allows
-      you to connect your games together into a campaign, with a "living" script
-      that changes every game. With this integration, ClockTracker now supports
-      a built-in method of supporting Living Scripts, by importing the campaign
-      and script information directly.
-    </p>
-    <p class="p-2">
-      When you are adding a game, you can now select to use a Living Script
-      campaign from the script selection dialog. There are two options: Classic
-      (a standard script) or Living Scripts.
-    </p>
-    <img src="/img/ls-1.png" />
-    <p class="p-2">
-      Once you select a Living Script campaign, you will be able to choose from
-      the available scripts in that campaign. Details such as the notes and the
-      game date, will be automatically filled in, and the custom background
-      image (if any) will be used for the game session.
-    </p>
-    <img src="/img/ls-2.png" />
-    <p class="p-2">
-      You can also add a Living Scripts game directly to ClockTracker usign the
-      provided button on the Living Scripts website.
-    </p>
-    <img src="/img/ls-3.png" />
-    <p class="p-2">
-      Living Scripts is an exciting, new way to play ClockTower, and I highly
-      recommend you check it out! The website is
-      <a
-        href="https://fleshandbone.app/"
-        target="_blank"
-        class="text-violet-600 hover:text-violet-800 dark:text-violet-400 dark:hover:text-violet-600"
-        >fleshandbone.app</a
-      >.
-    </p>
-    <p class="p-2">
-      Please let me know on Discord if you have any issues or feedback. Thank
-      you!
-    </p>
+    <div class="p-2">
+      <Token
+        size="lg"
+        :character="{
+          name: 'Hermit',
+          alignment: 'GOOD',
+          role: {
+            token_url: '/img/role/hermit.png',
+            type: 'Outsider',
+            initial_alignment: 'GOOD',
+            name: 'Hermit',
+          },
+        }"
+        class="m-auto"
+      />
+      <p class="text-center text-lg font-bold p-2">
+        You have all Outsider abilities. [-0 or -1 Outsider]
+      </p>
+      <p class="text-center italic w-2/3 m-auto text-sm">
+        In the lost and forgotten places of the earth, the soul’s light beckons.
+      </p>
+      <p class="p-2 pt-6">
+        The Hermit has now been added to ClockTracker! If you haven't read about
+        it already, check it out on
+        <a
+          href="https://wiki.bloodontheclocktower.com/Hermit"
+          target="_blank"
+          class="underline"
+          >the official Wiki page.</a
+        >
+        As games are played, you can see data on how it's playing on
+        <NuxtLink to="/roles/hermit" class="underline" target="_blank">
+          its ClockTracker page.
+        </NuxtLink>
+      </p>
+    </div>
   </AnnouncementDialog>
   <VitePwaManifest />
 </template>
@@ -91,6 +68,7 @@ import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
 import type { User } from "~/composables/useUsers";
+import { nanoid } from "nanoid";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
@@ -191,7 +169,7 @@ watch(
   }
 );
 
-const announcementDate = dayjs.tz("2025-02-03", "America/Los_Angeles");
+const announcementDate = dayjs.tz("2025-06-13", "America/Los_Angeles");
 const maintenanceMode = featureFlags.maintenanceIsScheduled;
 
 const formattedAnnouncementDate = computed(() => {
