07149e41ebc1c94be190278c2c653219fa1f3880
steven.d <dllrt@hotmail.com>
2025-12-05 20:41:36 +0100

    Replace svgs from games_rework with new IconUI

--- components/GameOverviewList.vue
+++ components/GameOverviewList.vue
@@ -133,9 +133,7 @@
                 }, played on ${formatDate(game.date)}`"
                 :to="`/game/${game.id}/edit`"
                 >
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                  <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
-                </svg>
+                <IconUI id="edit" :rounded="true" />
               </nuxt-link>
               <a
                 v-if="game.bgg_id"
@@ -148,6 +146,7 @@
                 }, played on ${formatDate(game.date)}`"
                 :href="`https://boardgamegeek.com/play/details/${game.bgg_id}`"
               >
+                <!-- @todo use IconUI-->
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 25.4 37.9" aria-hidden="true">
                   <path fill="currentColor" d="m24.9 7-3.8 1 3.7-8L.9 8.8l1.3 10.5L0 21.5l6.6 16.4 14-5.1 4.8-11.4-2.1-2L24.9 7z"/>
                 </svg>
@@ -162,10 +161,7 @@
                 }, played on ${formatDate(game.date)}`"
                 :to="getGameLink(game)"
               >
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                  <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/>
-                  <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
-                </svg>
+                <IconUI id="view" :rounded="true" />
               </nuxt-link>
             </div>
           </td>
