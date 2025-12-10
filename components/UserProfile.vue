aaebf1f32b6ca3bcddee2c148d9c1a5a3357521b
steven.d <dllrt@hotmail.com>
2025-12-10 09:57:09 +0100

    Remove unused cardWidth

--- components/UserProfile.vue
+++ components/UserProfile.vue
@@ -34,7 +34,6 @@
       <template v-if="favoriteGames.length > 0">
         <GameOverviewGrid
           :games="favoriteGames"
-          cardWidth="w-1/2 lg:w-1/4"
         />
         <div v-if="isMe" class="flex justify-center items-center">
           <button
