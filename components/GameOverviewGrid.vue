067ba03dc5e9017bb9f9efc8d9624bb279014976
steven.d <dllrt@hotmail.com>
2025-12-10 12:06:45 +0100

    Fix tagged players on card

--- components/GameOverviewGrid.vue
+++ components/GameOverviewGrid.vue
@@ -141,9 +141,8 @@
                 d="M64.12 147.8a4 4 0 0 1-4 4.2H16a8 8 0 0 1-7.8-6.17a8.35 8.35 0 0 1 1.62-6.93A67.8 67.8 0 0 1 37 117.51a40 40 0 1 1 66.46-35.8a3.94 3.94 0 0 1-2.27 4.18A64.08 64.08 0 0 0 64 144c0 1.28 0 2.54.12 3.8m182-8.91A67.76 67.76 0 0 0 219 117.51a40 40 0 1 0-66.46-35.8a3.94 3.94 0 0 0 2.27 4.18A64.08 64.08 0 0 1 192 144c0 1.28 0 2.54-.12 3.8a4 4 0 0 0 4 4.2H240a8 8 0 0 0 7.8-6.17a8.33 8.33 0 0 0-1.63-6.94Zm-89 43.18a48 48 0 1 0-58.37 0A72.13 72.13 0 0 0 65.07 212A8 8 0 0 0 72 224h112a8 8 0 0 0 6.93-12a72.15 72.15 0 0 0-33.74-29.93Z"
               />
             </svg>
-            <div class="text-white font-bold flex items-center gap-2 min-w-[50px] text-xs md:text-base">
+            <div class="text-white font-bold flex items-center gap-2">
               <span>{{ game.player_count }}</span>
-              <span class="hidden md:inline">
                 <template v-if="game.traveler_count">
                   (+{{ game.traveler_count }})
                 </template>
@@ -156,8 +155,7 @@
                     size="xs"
                     :value="player?.avatar"
                     class="bg-stone-300 dark:bg-stone-950"
-                  /></div
-              ></span>
+                  /></div>
             </div>
           </div>
           <div class="flex flex-grow justify-between gap-1 p-1">
