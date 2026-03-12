bb3a437a384d5f5b8f9e47a107348d7fde84dcc0
Lindsay Wardell <lindsay.wardell@mangomint.com>
2026-03-12 15:53:30 -0700

    Markdown stuff

---
+++ components/MarkdownRenderer.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import VueMarkdown from "vue-markdown-render";
+import { markdownAlignment } from "~/utils/markdownAlignment";
+
+defineProps<{
+  source: string;
+  options?: Record<string, any>;
+}>();
+
+const plugins = [markdownAlignment];
+</script>
+
+<template>
+  <VueMarkdown :source="source" :options="options" :plugins="plugins" />
+</template>
